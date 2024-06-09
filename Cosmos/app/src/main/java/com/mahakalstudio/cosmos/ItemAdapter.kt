a62cf7afb1fa1b1be52813b69179f5faef80c058
Deeepak <149338003+Deepak-Deepak31@users.noreply.github.com>
2024-06-09 16:51:24 -0400

    Browse wallpaper. fixed #13

---
+++ Cosmos/app/src/main/java/com/mahakalstudio/cosmos/ItemAdapter.kt
@@ -0,0 +1,30 @@
+package com.mahakalstudio.cosmos
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mahakalstudio.cosmos.databinding.ItemLayoutBinding
+
+class ItemAdapter(private val itemList: List<Pair<Item, Item>>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    inner class ItemViewHolder(private val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(itemPair: Pair<Item, Item>) {
+            val (item1, item2) = itemPair
+            binding.itemImage1.setImageResource(item1.imageResId)
+            binding.itemText1.text = item1.name
+            binding.itemImage2.setImageResource(item2.imageResId)
+            binding.itemText2.text = item2.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int = itemList.size
+}
